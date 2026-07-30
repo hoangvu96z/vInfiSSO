cd81d3f810b81b40843f006b4607f6ce8a265121
Vu Nguyen <hoangvu96z@gmail.com>
2026-07-30 11:22:19 +0700

    Refactor code structure for improved readability and maintainability

--- dist/auth/dto/create-login.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreateLoginDto {
-    username: string;
-    password: string;
-    appId?: string;
-    appName?: string;
-    notes?: string;
-}
--- dist/member-app/dto/create-member-app.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CreateMemberAppDto {
-    appName: string;
-    appId?: string;
-    memberName: string;
-    email: string;
-    role?: string;
-    status?: string;
-}
